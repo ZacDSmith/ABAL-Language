1d5327d041ad00550182bd24dc3629367cb32dd4
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-10-02 18:46:29 -0400

    Tree

--- src/cum/TokenType.java
+++ src/cum/TokenType.java
@@ -1,6 +1,6 @@
 package cum;
 enum TokenType {
-    // SIngle-character tokens.
+    // Single-character tokens.
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
